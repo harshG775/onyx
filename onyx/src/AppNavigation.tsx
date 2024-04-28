7d2d22d4d30700399b2ce14743e44fdd24a90c3f
Harsh <hgaur491@gmail.com>
2024-04-28 12:56:24 +0530

    installing // @react-navigation/native-stack

--- onyx/src/AppNavigation.tsx
+++ onyx/src/AppNavigation.tsx
@@ -8,17 +8,34 @@ import Home from "./screens/Home";
 
 function BottomTabNavigator() {
     return (
-        <BottomTab.Navigator>
+        <BottomTab.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
             <BottomTab.Screen name="Home" component={Home} />
+            <BottomTab.Screen name="Discover" component={Home} />
+            <BottomTab.Screen name="List" component={Home} />
+            <BottomTab.Screen name="Settings" component={Home} />
         </BottomTab.Navigator>
     );
 }
+// BottomTab navigation
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const Stack = createNativeStackNavigator();
 
+function StackNavigator() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="LOGO" component={BottomTabNavigator} />
+        </Stack.Navigator>
+    );
+}
 // Main navigation
 export default function AppNavigation() {
     return (
         <NavigationContainer>
-            <BottomTabNavigator />
+            <StackNavigator />
         </NavigationContainer>
     );
 }
