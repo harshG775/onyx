7013e8174dac05a780bd5cd80980a02ac37713cc
Harsh <hgaur491@gmail.com>
2024-05-16 14:27:54 +0530

    //// deleting old app

--- onyx/src/utils/env/env.server.tsx
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-// server env
-const envSchema = z.object({
-    CONSUMET_BASEURL: z.string(),
-});
-export const envServer = envSchema.parse({
-    CONSUMET_BASEURL: process.env.CONSUMET_BASEURL,
-});
